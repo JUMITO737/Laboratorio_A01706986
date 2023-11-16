a8c32fd3a1cf5f42a45d450fc259e5ffa3e94967
JUMITO737 <145620410+JUMITO737@users.noreply.github.com>
2023-11-15 19:21:41 -0600

    Add files via upload

---
+++ Ejercicio (1).py
@@ -0,0 +1,9 @@
+import datetime
+import time
+import pandas as pd
+import streamlit as st
+from PIL import Image
+
+st.title("Titulo: Analitica de Datos")
+st.header("Este es un header")
+st.subheader("Este es un subheader")
